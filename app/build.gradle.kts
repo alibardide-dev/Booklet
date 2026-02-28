ae2ff6e4be6c162b5be5e5db3d56d27044e05351
Ali Bardide <alibardide5124@gmail.com>
2026-02-28 10:51:22 +0330

    Upgrade libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.google.ksp)
@@ -60,7 +59,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.material3)
 
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.splash)
